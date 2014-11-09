3f765604228124c0286baa3867d3a9a95e292a60
ishaklive <ishak.live@gmail.com>
2014-11-09 13:42:23 +0530

    Update the Correct ProtocolSelect.py
    ProtocolSelected.py file originally added by chathura is not the
    correct. I removed it and add the correct file

--- assets/ProtocolSelector.py
+++ assets/ProtocolSelector.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python
 
-#from DataReader import *
+#from protocolData import ProtocolData
 
-
+import xml.etree.ElementTree as ET
 
 class ProtocolSelector:
   
@@ -23,96 +23,161 @@ class ProtocolSelector:
   	transport_features=0x0
   	protcol_data=None
   
+        def __init__(self):
+                self.protcol_data=ProtocolData('/data/data/chathura.research/files/data.xml')
+                #we have to give the direct path to the xml on the mobile
   	
-    	def Add(x,y) :
+    	def Add(self,x,y) :
                 return x+y
   
         
   	def setTransportFeatures(self,features): # 5= 1+4, reliable and high frequency
                 print 'Setting transport feature',features
                 self.transport_features=features
-        '''
+        
   	def selectProtocol(self):
-  	self.protcol_data=ProtocolData('data.xml')
-    	protocol=''
-    	packetSize=0
-    	if(self.transport_features&self.RELIABLE):
-      		#reliable protocols are TCP and SCTP
+  	
+                protocol=''
+                packetSize=0
+                if(self.transport_features&self.RELIABLE):
+                        #reliable protocols are TCP and SCTP
       
-	      	if(self.transport_features&self.LARGE_DATA):
-	        	#print 'reliable large'
-	        	packetSize='1024'
+                        if(self.transport_features&self.LARGE_DATA):
+                                #print 'reliable large'
+                                packetSize='1024'
 
-	      	elif(self.transport_features&self.MEDIUM_DATA):
-	        	#print 'reliable medium'
-	        	packetSize='500'
+                        elif(self.transport_features&self.MEDIUM_DATA):
+                                #print 'reliable medium'
+                                packetSize='500'
 
-	      	elif(self.transport_features&self.SMALL_DATA):
-	        	#print 'reliable small'
-	        	packetSize='100'
-
-	      	elif(self.transport_features&self.VERY_SMALL_DATA):
-	        	#print 'reliable very small'
-	        	packetSize='10'
-      
-	      	tcp_throughput=float(self.protcol_data.getThroughput('TCP',packetSize))
-	      	sctp_throughput=float(self.protcol_data.getThroughput('SCTP',packetSize))
+                      	elif(self.transport_features&self.SMALL_DATA):
+                        	#print 'reliable small'
+                                packetSize='100'
+        
+                      	elif(self.transport_features&self.VERY_SMALL_DATA):
+                        	#print 'reliable very small'
+                                packetSize='10'
+              
+                      	tcp_throughput=float(self.protcol_data.getThroughput('TCP',packetSize))
+                        sctp_throughput=float(self.protcol_data.getThroughput('SCTP',packetSize))
 	      
-	      	tcp_delay=float(self.protcol_data.getAverageDelay('TCP',packetSize))
-	      	sctp_delay=float(self.protcol_data.getAverageDelay('SCTP',packetSize))
+                        tcp_delay=float(self.protcol_data.getAverageDelay('TCP',packetSize))
+                      	sctp_delay=float(self.protcol_data.getAverageDelay('SCTP',packetSize))
 	      
-	      	#selecting based on throughput
-	      	if(tcp_throughput>sctp_throughput):
-	        	print 'TCP SELECTED'
-	        	protocol='TCP'
-	      	else:
-	        	print 'SCTP selected'
-	        	protocol='SCTP'
+                      	#selecting based on throughput
+                      	if(tcp_throughput>sctp_throughput):
+                        	print 'TCP SELECTED'
+                        	protocol='TCP'
+                      	else:
+                               	print 'SCTP selected'
+                        	protocol='SCTP'
   
   
-    	elif(self.transport_features&self.UNRELIABLE):
-      		#unreliables are DCCP and UDP
-      		if(self.transport_features&self.LARGE_DATA):
-        		#print 'reliable large'
-        		packetSize='1024'
+                elif(self.transport_features&self.UNRELIABLE):
+                	#unreliables are DCCP and UDP
+                	if(self.transport_features&self.LARGE_DATA):
+                        	#print 'reliable large'
+                        	packetSize='1024'
 
-      		elif(self.transport_features&self.MEDIUM_DATA):
-        		#print 'reliable medium'
-        		packetSize='500'
+                        elif(self.transport_features&self.MEDIUM_DATA):
+                        	#print 'reliable medium'
+                        	packetSize='500'
 
-      		elif(self.transport_features&self.SMALL_DATA):
-        		#print 'reliable small'
-        		packetSize='100'
+                        elif(self.transport_features&self.SMALL_DATA):
+                        	#print 'reliable small'
+                        	packetSize='100'
 
-      		elif(self.transport_features&self.VERY_SMALL_DATA):
-        		#print 'reliable very small'
-        		packetSize='10'
+                        elif(self.transport_features&self.VERY_SMALL_DATA):
+                        	#print 'reliable very small'
+                        	packetSize='10'
       
-      		udp_throughput=float(self.protcol_data.getThroughput('UDP',packetSize))
-      		dccp_throughput=float(self.protcol_data.getThroughput('DCCP',packetSize))
+                        udp_throughput=float(self.protcol_data.getThroughput('UDP',packetSize))
+                        dccp_throughput=float(self.protcol_data.getThroughput('DCCP',packetSize))
       
-      		udp_delay=float(self.protcol_data.getAverageDelay('UDP',packetSize))
-      		dccp_delay=float(self.protcol_data.getAverageDelay('DCCP',packetSize))
+                        udp_delay=float(self.protcol_data.getAverageDelay('UDP',packetSize))
+                        dccp_delay=float(self.protcol_data.getAverageDelay('DCCP',packetSize))
       
-	      	#selecting based on throughput
-	      	if(dccp_throughput>udp_throughput):
-	        	print 'DCCP SELECTED'
-	        	protocol='DCCP'
-	      	else:
-	        	print 'UDP selected'
-	        	protocol='UDP'
-    	return protocol
-    	'''
+                      	#selecting based on throughput
+                      	if(dccp_throughput>udp_throughput):
+                                print 'DCCP SELECTED'
+                                protocol='DCCP'
+                        else:
+                                print 'UDP selected'
+                                protocol='UDP'
+                return protocol
+    	
 #end def select
     
+class ProtocolData:
+  throughput=dict()
+  averageDelay=dict()
+  dropRatio=dict()
+  xmldoc=None
+
+  def __init__(self,dataFile):
+    self.setDataFile(dataFile)
+    self.loadData()
+    
+
+  def setDataFile(self,dataFile):
+    if(dataFile):
+      self.xmldoc=ET.parse(dataFile)   
+    else:
+      print "Invalid data file path"
+  
+  
+  def loadData(self):
+    root=self.xmldoc.getroot()#data tag
+    protocol_list=["TCP","UDP","DCCP","SCTP"]
+    
+    for protocol in protocol_list:
+      #print "getting data for the protocol ",protocol
+      xpath_string_throughput="./Protocol/"+protocol+"/Node/Throughput/packet"
+      xpath_string_delays="./Protocol/"+protocol+"/Node/AverageDelay/packet"
+      xpath_string_drop="./Protocol/"+protocol+"/Node/DropRatio/packet"
+      #print "XPath for ",protocol, xpath_string_delays
+      
+      self.throughput[protocol]=dict()
+      self.averageDelay[protocol]=dict()
+      self.dropRatio[protocol]=dict()
+      
+      
+      for throughputs in root.findall(xpath_string_throughput):
+	packetSize=throughputs.attrib['size']
+	thrput=throughputs.text
+	self.throughput[protocol][packetSize]=thrput
+      
+      for delays in root.findall(xpath_string_delays):
+	packetSize=delays.attrib['size']
+	delay=delays.text
+	self.averageDelay[protocol][packetSize]=delay
+      
+      
+      for drops in root.findall(xpath_string_drop):
+	packetSize=drops.attrib['size']
+	drop=drops.text
+	self.dropRatio[protocol][packetSize]=drop
+
+
+  def getThroughput(self,protocol,packetSize):
+    if(self.throughput):
+      return self.throughput[protocol][packetSize];
+    
+  def getAverageDelay(self,protocol,packetSize):
+    if(self.averageDelay):
+      return self.averageDelay[protocol][packetSize]
+    
+  def getDropRatio(self,protocol,packetSize):
+    if(self.dropRatio):
+      return self.dropRatio[protocol][packetSize];
 
     
 def main():
 	p=ProtocolSelector()
-	#p.setTransportFeatures(p.UNRELIABLE|p.MEDIUM_DATA)
-	#selected_protocol=p.selectProtocol()
-        d=self.Add(10,30)
-        return d
+	p.setTransportFeatures(p.RELIABLE|p.SMALL_DATA)
+	selected_protocol=p.selectProtocol()
+        #d=p.Add(10,30)
+        return selected_protocol
   
  # p.setTransportFeatures(p.RELIABLE|p.SMALL_DATA)
  #selected_protocol=p.selectProtocol()
